5e2a0e2e3455b5fdba6be50c1426f2d186667fec
魂归梓里 <ziazusa@foxmail.com>
2023-02-23 23:57:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ addEventListener('fetch', event => {
     return event.respondWith(handle(event));
 });
 async function handle(event) {
+    //添加跨域和缓存控制响应头
     const resHdrs = new Headers({
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': 'true',
@@ -11,6 +12,7 @@ async function handle(event) {
     });
     const cache = caches.default;
     const url = new URL(event.request.url);
+    //处理新浪图床链接
     let sinaUrl = String(url.pathname).slice(1);
     if (sinaUrl.startsWith("http")) {
         sinaUrl = sinaUrl.replace(/https:(\/)*/,'').replace(/http:(\/)*/, '');
@@ -19,6 +21,7 @@ async function handle(event) {
     if (!newUrl.hostname.includes("sinaimg.cn")) {
         return new Response('This is a Teapot', {status: 418});
     }
+    //检查Cloudflare缓存
     let response = await cache.match(newUrl);
     if (response) {
         resHdrs.set('X-Worker-Cache', "true");
@@ -29,6 +32,7 @@ async function handle(event) {
             headers: resHdrs
         });
     }
+    //回源新浪图床
     response = await fetch(newUrl, {
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0 Win64 x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41",
@@ -45,6 +49,7 @@ async function handle(event) {
         statusText: response.statusText,
         headers: resHdrs
     });
+    //写入Cloudflare缓存
     event.waitUntil(cache.put(newUrl, response.clone()));
     return response;
 }
